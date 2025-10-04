32ec7e09a9dc533206d3e78c3bb24b4571984b85
Rythemenz E <rythemenze@gmail.com>
2025-10-04 18:07:25 +0000

    REDUCE THIS FORM TO A MINIMAL TYPE AT THE CENTER  (_for element div_)

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -80,9 +80,6 @@ export default function LoginPage() {
             <div className="mx-auto grid w-full gap-6">
               <div className="grid gap-2 text-center">
                 <h1 className="font-headline text-3xl">Login</h1>
-                <p className="text-balance text-muted-foreground">
-                  Enter your email below to login to your account
-                </p>
               </div>
               <LoginForm />
               <div className="mt-4 text-center text-sm">
